217fe2f549f5e2a4ce12e2d3aec9a2c38b658cb9
jeff souza <jeffreymsouza@gmail.com>
2017-02-16 03:02:50 -0700

    p4

--- a2/problem_4.c
+++ a2/problem_4.c
@@ -1,6 +1,17 @@
+#include "pthread.h"
+#include "stdio.h"
+#include "assert.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+int num_threads;
+int run_time;
+
+int volatile continue_running;
+
 struct spin_lock_t {
   volatile int lock;
-}
+};
 
 /*
  * atomic_cmpxchg
@@ -25,9 +36,48 @@ static inline int atomic_cmpxchg (volatile int *ptr, int old, int new) {
 }
 
 void spin_lock (struct spin_lock_t *s) {
-  while(atomic_cmpxchg(s,s->lock,1));
+  while(atomic_cmpxchg(&(s->lock),s->lock,1));
 }
 
 void spin_unlock (struct spin_lock_t *s) {
   s->lock = 0;
+}
+
+void* thread_start(void* arg) {
+  (void)(arg); // get rid of unused warning
+
+  struct spin_lock_t sl;
+  sl.lock = 0;
+  spin_lock(&sl);
+  spin_unlock(&sl);
+
+  return NULL;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    printf("Wrong number of arguments\n");
+    return 1;
+  }
+
+  num_threads = atoi(argv[1]);
+  run_time = atoi(argv[2]);
+
+  continue_running = 1;
+
+  pthread_t threads[num_threads];
+
+  int i;
+  for (i=0; i<num_threads; ++i) {
+    pthread_create(&threads[i], NULL, &thread_start, NULL);
+  }
+
+  sleep(run_time);
+  continue_running = 0;
+
+  for (i=0; i<num_threads; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+
+  return 0;
 }
